dcedc7ec17a4e0b3f4a879bb8fb880c8067b2992
pachipico <pachipico1@gmail.com>
2023-10-25 09:45:25 +0900

    asdf

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import styled from "styled-components";
 import useSWR, { useSWRConfig } from "swr";
 import Board from "../../components/Board";
@@ -19,31 +19,33 @@ const BoardContainer = styled.div`
 `;
 
 const Home = () => {
-  const { data: tokens } = useSWR("http://localhost:8080/api/v1/reissue", reissueFetcher, {
-    dedupingInterval: 1000 * 60 * 30,
-  });
+  // const { data: tokens } = useSWR("http://localhost:8080/api/v1/reissue", reissueFetcher, {
+  //   dedupingInterval: 1000 * 60 * 30,
+  // });
+  // { bid, title, writer, regAt } 
+  const [boardData, setBoardData] = useState([{bid:1, title:"title", writer:"writer", regAt:"regAt"}, {bid:1, title:"title", writer:"writer", regAt:"regAt"}, {bid:1, title:"title", writer:"writer", regAt:"regAt"}, {bid:1, title:"title", writer:"writer", regAt:"regAt"}]);
 
-  const { data: boardData } = useSWR("http://localhost:8080/api/v1/boards", fetcher);
+  
 
-  const jwt = useCallback(() => {
-    // let decoded = jwt_decode(tokens.accessToken);
-    console.log(document.cookie);
-    console.log(tokens);
-    const accessToken = getCookie("accessToken");
-    const refreshToken = getCookie("refreshToken");
-    if (accessToken && refreshToken) {
-      console.log("cookie access", accessToken, jwtDecode(accessToken));
-      console.log("cookie refresh", refreshToken, jwtDecode(refreshToken));
-    }
-  }, [tokens]);
+  // const jwt = useCallback(() => {
+  //   // let decoded = jwt_decode(tokens.accessToken);
+  //   console.log(document.cookie);
+  //   console.log(tokens);
+  //   const accessToken = getCookie("accessToken");
+  //   const refreshToken = getCookie("refreshToken");
+  //   if (accessToken && refreshToken) {
+  //     console.log("cookie access", accessToken, jwtDecode(accessToken));
+  //     console.log("cookie refresh", refreshToken, jwtDecode(refreshToken));
+  //   }
+  // }, [tokens]);
 
   return (
     <Container>
-      <button onClick={() => console.log(tokens)}>로그인중인가?</button>
-      <button onClick={jwt}>토큰?</button>
+      {/* <button onClick={() => console.log(tokens)}>로그인중인가?</button> */}
+      {/* <button onClick={jwt}>토큰?</button> */}
       {/* <button onClick={reissue}>reissue?</button> */}
       <BoardContainer>
-        {boardData?.list.map((b) => {
+        {boardData?.map((b) => {
           return <Board board={b} />;
         })}
       </BoardContainer>
